663ad912f6b5be276b3304a4534caf97ac3f9d6b
nakazy <s.nakajima0523@gmail.com>
2016-02-17 22:53:41 +0900

    Initial Commit

---
+++ SwiftServerVapor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftServerVapor
+//
+//  Created by nakazy on 2016/02/17.
+//  Copyright © 2016年 nakazy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
